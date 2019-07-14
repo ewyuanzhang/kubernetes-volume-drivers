8e373f187f1f739ba1b62e29ddbd8d138901cd87
Yuan Zhang <ewyuanzhang@gmail.com>
2019-07-14 02:47:31 -0700

    Add a blobfuse binary for customizable endpoint

--- flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
+++ flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
@@ -10,10 +10,26 @@ fi
 
 echo "begin to install blobfuse FlexVolume driver ${VER}, target dir:${target_dir} ..." >> $LOG
 
+#install jq
+apt-get install jq cifs-utils -y
+
+#setup blobfuse volume directory
 blobfuse_vol_dir="${target_dir}/azure~blobfuse"
 mkdir -p ${blobfuse_vol_dir} >>$LOG 2>&1
 
+#download blobfuse binary and lib
+wget -O ${blobfuse_vol_dir}/release.json https://api.github.com/repos/ewyuanzhang/azure-storage-fuse/releases/latest >>$LOG 2>&1
+blobfuse_url=`cat ${blobfuse_vol_dir}/release.json | grep "browser_download_url" | cut -d '"' -f 4` >>$LOG 2>&1
+wget -O ${blobfuse_vol_dir}/blobfuse.zip ${blobfuse_url} >>$LOG 2>&1
+unzip -d ${blobfuse_vol_dir} ${blobfuse_vol_dir}/blobfuse.zip >>$LOG 2>&1
+blobfuse_lib_dir="${blobfuse_vol_dir}/lib"
+blobfuse_bin_dir="${blobfuse_vol_dir}/bin"
+chmod a+x ${blobfuse_bin_dir}/blobfuse >>$LOG 2>&1
+rm ${blobfuse_vol_dir}/release.json ${blobfuse_vol_dir}/blobfuse.zip >>$LOG 2>&1
+
 #copy blobfuse script
+sed -i "s,BLOBFUSE_BIN=\"\",BLOBFUSE_BIN=\"$blobfuse_bin_dir/blobfuse\",g" /blobfuse/blobfuse
+sed -i "s,BLOBFUSE_LIB=\"\",BLOBFUSE_LIB=\"$blobfuse_lib_dir\",g" /blobfuse/blobfuse
 cp /blobfuse/blobfuse ${blobfuse_vol_dir}/blobfuse >>$LOG 2>&1
 chmod a+x ${blobfuse_vol_dir}/blobfuse >>$LOG 2>&1
 
